f75f485d0a768fc64019d4fb025e3b8b80facba9
unknown <dyahxad.s+sbalzammo@gmail.com>
2023-05-29 15:45:21 +0700

    first commit

---
+++ Character Animator Project/Ch Data/temp.noindex/compiler/bootstrap.js
@@ -0,0 +1,3 @@
+var __bootStrapPath = _modulePath;
+var __compiledScene = _cwd + "\\scene.json";
+runScript("D:\\PortableApps\\Adobe Character Animator 2022\\Support Files\\Runtime\\animal\\scripts\\js\\lib\\engine.js")
